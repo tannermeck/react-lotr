9d2a82b8e94b7ca7a1f040f50b6946d32a0ff246
TANNER MECK <steve.meck246@gmail.com>
2022-03-21 17:44:11 -0700

    everything rendering

--- src/views/Books/Books.js
+++ src/views/Books/Books.js
@@ -5,14 +5,18 @@ export default function Books() {
   const [books, setBooks] = useState([]);
   useEffect(() => {
     const fetch = async () => {
-      const books = await fetchBooks();
-      console.log(books);
+      const booksData = await fetchBooks();
+      console.log(booksData);
+      setBooks(booksData);
     };
     fetch();
   }, []);
   return (
     <>
       <h1>Books</h1>
+      {books.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
     </>
   );
 }
